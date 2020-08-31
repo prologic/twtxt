87a8b37fa053ada8c229e869f1cad9e90cbf8b48
James Mills <prologic@shortcircuit.net.au>
2020-08-31 19:28:39 +1000

    Fix tests

--- types/twt_test.go
+++ types/twt_test.go
@@ -2,7 +2,7 @@ package types
 
 import (
 	"testing"
-  "time"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -11,28 +11,49 @@ func TestSubject(t *testing.T) {
 	assert := assert.New(t)
 
 	t.Run("String", func(t *testing.T) {
-		//f := Feed{Nick: "prologic", URL: "https://twtxt.net/user/prologic/twtxt.txt"}
-		//assert.Equal("@<prologic https://twtxt.net/user/prologic/twtxt.txt>", f.String())
-    // a := [5][2]string{{"@antonio (#iuf98kd) nice post!","(#iuf98kd)"}, {"",""}, {"",""}, {"",""},{"",""}}
+		testCases := []struct {
+			Text    string
+			Subject string
+		}{
+			{
+				Text:    "@<antonio bla.com> (#iuf98kd) nice post!",
+				Subject: "(#iuf98kd)",
+			}, {
+				Text:    "@<prologic bla.com> (re nice jacket)",
+				Subject: "(re nice jacket)",
+			}, {
+				Text:    "(re nice jacket)",
+				Subject: "(re nice jacket)",
+			}, {
+				Text:    "Best time of the week (aka weekend)",
+				Subject: "",
+			}, {
+				Text:    "@<antonio bla.com> (re weekend) I like the weekend too. (is the best)",
+				Subject: "(re weekend)",
+			}, {
+				Text:    "tomorrow (sat) (sun) (moon)",
+				Subject: "",
+			}, {
+				Text:    "@<antonio2 bla.com> @<antonio bla.com> (#j3hyzva) testte #test1 (s) and #test2 (s) and more text",
+				Subject: "(#j3hyzva)",
+			}, {
+				Text:    "@<antonio3 bla.com> @<antonio bla.com> (#j3hyzva) testing again",
+				Subject: "(#j3hyzva)",
+			}, {
+				Text:    "(#veryfunny) you are funny",
+				Subject: "(#veryfunny)",
+			}, {
+				Text:    "#having fun (satruday) another day",
+				Subject: "",
+			}, {
+				Text:    "@<antonio3 bla.com> not funny dude",
+				Subject: "",
+			},
+		}
 
-    cases := [][]string{
-		              []string{"@<antonio bla.com> (#iuf98kd) nice post!", "(#iuf98kd)"},
-		              []string{"@<prologic bla.com> (re nice jacket)", "(re nice jacket)"},
-		              []string{"(re nice jacket)", ""},
-                  []string{"Best time of the week (aka weekend)", ""},
-                  []string{"@<antonio bla.com> (re weekend) I like the weekend too. (is the best)", "(re weekend)"},
-                  []string{"tomorrow (sat) (sun) (moon)",""},
-                  []string{"@<antonio2 bla.com> @<antonio bla.com> (#j3hyzva) testte #test1 (s) and #test2 (s) and more text","(#j3hyzva)"},
-                  []string{"@<antonio3 bla.com> @<antonio bla.com> (#j3hyzva) testing again", "(#j3hyzva)"},
-                  []string{"(#veryfunny) you are funny",""},
-                  []string{"#having fun (satruday) another day",""},
-                  []string{"@<antonio3 bla.com> not funny dude",""},
-	               }
-
-    for i := 0; i < len(cases); i++ {
-      twt := Twt{Twter: Twter{}, Text: cases[i][0], Created: time.Now()}
-      sub := twt.Subject()
-      assert.Equal(cases[i][1], sub)
-    }
+		for _, testCase := range testCases {
+			twt := Twt{Twter: Twter{}, Text: testCase.Text, Created: time.Now()}
+			assert.Equal(testCase.Subject, twt.Subject())
+		}
 	})
 }

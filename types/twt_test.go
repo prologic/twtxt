0900ca8106468cf34170bac7e20a886b905894a3
Antonio Rodrigues <aaadonai@gmail.com>
2020-08-30 23:49:18 -0700

    Added twt_test.go with test cases for Subject()

---
+++ types/twt_test.go
@@ -0,0 +1,37 @@
+package types
+
+import (
+	"testing"
+  "time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSubject(t *testing.T) {
+	assert := assert.New(t)
+
+	t.Run("String", func(t *testing.T) {
+		//f := Feed{Nick: "prologic", URL: "https://twtxt.net/user/prologic/twtxt.txt"}
+		//assert.Equal("@<prologic https://twtxt.net/user/prologic/twtxt.txt>", f.String())
+    // a := [5][2]string{{"@antonio (#iuf98kd) nice post!","(#iuf98kd)"}, {"",""}, {"",""}, {"",""},{"",""}}
+
+    cases := [][]string{
+		              []string{"@antonio (#iuf98kd) nice post!", "(#iuf98kd)"},
+		              []string{"@prologic (re nice jacket)", "(re nice jacket)"},
+		              []string{"(re nice jacket)", ""},
+                  []string{"Best time of the week (aka weekend)", ""},
+                  []string{"@antonio (re weekend) I like the weekend too. (is the best)", "(re weekend)"},
+                  []string{"tomorrow (sat) (sun) (moon)",""},
+                  []string{"@antonio2 @antonio (#j3hyzva) testte #test1 (s) and #test2 (s) and more text","(#j3hyzva)"},
+                  []string{"@antonio3 @antonio (#j3hyzva) testing again", "(#j3hyzva)"},
+                  []string{"(#veryfunny) you are funny",""},
+                  []string{"#having fun (satruday) another day",""},
+	               }
+
+    for i := 0; i < len(cases); i++ {
+      twt := Twt{Twter: Twter{}, Text: cases[i][0], Created: time.Now()}
+      sub := twt.Subject()
+      assert.Equal(cases[i][1], sub)
+    }
+	})
+}

e279d902a4223ce125def5d09d05ba66207daf9c
Antonio Rodrigues <aaadonai@gmail.com>
2020-08-31 00:07:36 -0700

    Fixed test cases with the @<user url> format

--- types/twt_test.go
+++ types/twt_test.go
@@ -16,16 +16,17 @@ func TestSubject(t *testing.T) {
     // a := [5][2]string{{"@antonio (#iuf98kd) nice post!","(#iuf98kd)"}, {"",""}, {"",""}, {"",""},{"",""}}
 
     cases := [][]string{
-		              []string{"@antonio (#iuf98kd) nice post!", "(#iuf98kd)"},
-		              []string{"@prologic (re nice jacket)", "(re nice jacket)"},
+		              []string{"@<antonio bla.com> (#iuf98kd) nice post!", "(#iuf98kd)"},
+		              []string{"@<prologic bla.com> (re nice jacket)", "(re nice jacket)"},
 		              []string{"(re nice jacket)", ""},
                   []string{"Best time of the week (aka weekend)", ""},
-                  []string{"@antonio (re weekend) I like the weekend too. (is the best)", "(re weekend)"},
+                  []string{"@<antonio bla.com> (re weekend) I like the weekend too. (is the best)", "(re weekend)"},
                   []string{"tomorrow (sat) (sun) (moon)",""},
-                  []string{"@antonio2 @antonio (#j3hyzva) testte #test1 (s) and #test2 (s) and more text","(#j3hyzva)"},
-                  []string{"@antonio3 @antonio (#j3hyzva) testing again", "(#j3hyzva)"},
+                  []string{"@<antonio2 bla.com> @<antonio bla.com> (#j3hyzva) testte #test1 (s) and #test2 (s) and more text","(#j3hyzva)"},
+                  []string{"@<antonio3 bla.com> @<antonio bla.com> (#j3hyzva) testing again", "(#j3hyzva)"},
                   []string{"(#veryfunny) you are funny",""},
                   []string{"#having fun (satruday) another day",""},
+                  []string{"@<antonio3 bla.com> not funny dude",""},
 	               }
 
     for i := 0; i < len(cases); i++ {

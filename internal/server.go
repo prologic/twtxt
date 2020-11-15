fc0158518e9ac0caf2887b7453e6e7ac27a349c0
James Mills <prologic@shortcircuit.net.au>
2020-11-15 15:32:47 +1000

    Add improved async media upload endpoint for the API (backwards compatible)

--- internal/server.go
+++ internal/server.go
@@ -656,6 +656,8 @@ func NewServer(bind string, options ...Option) (*Server, error) {
 
 	am := auth.NewManager(auth.NewOptions("/login", "/register"))
 
+	tasks := NewDispatcher(10, 100) // TODO: Make this configurable?
+
 	pm := passwords.NewScryptPasswords(nil)
 
 	sc := NewSessionStore(db, config.SessionCacheTTL)
@@ -670,7 +672,7 @@ func NewServer(bind string, options ...Option) (*Server, error) {
 		sc,
 	)
 
-	api := NewAPI(router, config, cache, archive, db, pm)
+	api := NewAPI(router, config, cache, archive, db, pm, tasks)
 
 	server := &Server{
 		bind:      bind,
@@ -709,7 +711,7 @@ func NewServer(bind string, options ...Option) (*Server, error) {
 		cron: cron.New(),
 
 		// Dispatcher
-		tasks: NewDispatcher(10, 100), // TODO: Make this configurable?
+		tasks: tasks,
 
 		// Auth Manager
 		am: am,

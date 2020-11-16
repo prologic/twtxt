e732238ad9a493c2654ed29ee198246dfd8fe55c
James Mills <prologic@shortcircuit.net.au>
2020-11-16 22:31:12 +1000

    Add message list stubs and initiate message form

--- internal/messages_handler.go
+++ internal/messages_handler.go
@@ -2,27 +2,55 @@ package internal
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/prologic/twtxt/types"
 	"github.com/vcraescu/go-paginator"
 	"github.com/vcraescu/go-paginator/adapter"
 )
+
+type Message struct {
+	User    string
+	Created time.Time
+	Updated time.Time
+
+	hash string
+}
+
+func (m Message) Hash() string {
+	if m.hash != "" {
+		return m.hash
+	}
+
+	return FastHash(m.User + "\n" + m.Created.String())
+}
+
+func (m Message) Text() string {
+	return "foo bar baz"
+}
+
+type Messages []Message
 
 // MessagesHandler ...
 func (s *Server) MessagesHandler() httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		ctx := NewContext(s.config, s.db, r)
 
-		twts := types.Twts{}
+		messages := Messages{
+			Message{
+				User:    "admin",
+				Created: time.Now(),
+				Updated: time.Now(),
+			},
+		}
 
-		var pagedTwts types.Twts
+		var pagedMessages Messages
 
 		page := SafeParseInt(r.FormValue("p"), 1)
-		pager := paginator.New(adapter.NewSliceAdapter(twts), s.config.TwtsPerPage)
+		pager := paginator.New(adapter.NewSliceAdapter(messages), s.config.TwtsPerPage)
 		pager.SetPage(page)
 
-		if err := pager.Results(&pagedTwts); err != nil {
+		if err := pager.Results(&pagedMessages); err != nil {
 			ctx.Error = true
 			ctx.Message = "An error occurred while loading search results"
 			s.render("error", w, ctx)
@@ -34,10 +62,12 @@ func (s *Server) MessagesHandler() httprouter.Handle {
 			return
 		}
 
-		title := "Messages ..."
+		title := "Messages"
 
 		ctx.Title = title
-		ctx.Twts = FilterTwts(ctx.User, pagedTwts)
+		// TODO: Filter out messages from blocked users?
+		//ctx.Messages = FilterMessages(ctx.User, pagedMessages)
+		ctx.Messages = pagedMessages
 		ctx.Pager = &pager
 		s.render("messages", w, ctx)
 		return

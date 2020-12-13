00e921e95ad04d074a5d5267c357eccd8a803828
James Mills <prologic@shortcircuit.net.au>
2020-12-13 12:09:27 +1000

    Add single message stub template and handler

--- internal/messages_handler.go
+++ internal/messages_handler.go
@@ -2,9 +2,34 @@ package internal
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+type Message struct {
+	From    string
+	Subject string
+	Sent    time.Time
+
+	hash string
+}
+
+func (m *Message) Hash() string {
+	if m.hash != "" {
+		return m.hash
+	}
+
+	m.hash = "fa47b31"
+
+	return m.hash
+}
+
+func (m *Message) Text() string {
+	return "Hello there!"
+}
+
+type Messages []*Message
 
 // MessagesHandler ...
 func (s *Server) MessagesHandler() httprouter.Handle {
@@ -13,6 +38,31 @@ func (s *Server) MessagesHandler() httprouter.Handle {
 
 		ctx.Title = "Private Messages"
 		s.render("messages", w, ctx)
+		return
+	}
+}
+
+// MessageHandler ...
+func (s *Server) MessageHandler() httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		ctx := NewContext(s.config, s.db, r)
+
+		if r.Method == http.MethodPost {
+			// TODO: Implement this :D
+			return
+		}
+
+		ctx.Title = "Private Message from Kate: Hello"
+
+		ctx.Messages = Messages{
+			&Message{
+				From:    "kate",
+				Subject: "Hello There!",
+				Sent:    time.Now(),
+			},
+		}
+
+		s.render("message", w, ctx)
 		return
 	}
 }
